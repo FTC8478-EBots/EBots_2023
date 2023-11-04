9d4a45014bf0f37a991f869ae80ecac362aedfa0
ebots8478 <146142210+ebots8478@users.noreply.github.com>
2023-11-04 15:40:32 -0400

    Software Updates Pre tuning, updated chassis.  About to calibrate arm and general motion

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+/**
+ * This is a simple teleop routine for testing localization. Drive the robot around like a normal
+ * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
+ * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
+ * exercise is to ascertain whether the localizer has been configured properly (note: the pure
+ * encoder localizer heading may be significantly off if the track width has not been tuned).
+ */
+@TeleOp(group = "drive")
+public class LocalizationTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int fl = 0, fr = 0, rl = 0, rr = 0;
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+
+        while (!isStopRequested()) {
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+
+            Pose2d poseEstimate = drive.getPoseEstimate();
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            if (gamepad1.square) {
+                fl = 1;
+                telemetry.addData("Frontleft on","");
+            } else {
+                fl = 0;
+            }
+            if (gamepad1.triangle) {
+                fr = 1;
+                telemetry.addData("Frontright on","");
+            } else {
+                fr = 0;
+            }
+            if (gamepad1.cross) {
+                rl = 1;
+                telemetry.addData("Rearleft on","");
+            } else {
+                rl = 0;
+            }
+            if (gamepad1.circle) {
+                rr = 1;
+                telemetry.addData("rearRight on","");
+            } else {
+                rr = 0;
+            }
+            drive.setMotorPowers(fl, rl, rr, fr);
+
+            telemetry.update();
+        }
+    }
+}
